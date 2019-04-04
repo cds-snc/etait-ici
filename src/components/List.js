3776244843441861a90450fd16bceee988deb397
Guillaume Charest <guillaume.w.charest@gmail.com>
2019-04-04 15:01:48 -0400

    Fix John's stuff (#36)
    I totally made that mistake on purpose, and committed to master for fun. :)

--- src/components/List.js
+++ src/components/List.js
@@ -2,29 +2,67 @@ import React from "react";
 import Name from "./Name";
 import "./List.css";
 
-const data = [
-  { name: "CDS-SNC", date: "12.02.2019", plural: true },
-  { name: "L'équipe de la Plateforme", date: "12.02.2019", plural: true },
-  { name: "Khalid", date: "13.02.2019", plural: false },
-  { name: "Dan", date: "13.02.2019", plural: false },
-  { name: "Élenchos", date: "11.03.2019", plural: false },
-  { name: "John O'Brien', date: "2019-04-04", plural: false}
+const data = [{
+    name: "CDS-SNC",
+    date: "12.02.2019",
+    plural: true
+  },
+  {
+    name: "L'équipe de la Plateforme",
+    date: "12.02.2019",
+    plural: true
+  },
+  {
+    name: "Khalid",
+    date: "13.02.2019",
+    plural: false
+  },
+  {
+    name: "Dan",
+    date: "13.02.2019",
+    plural: false
+  },
+  {
+    name: "Élenchos",
+    date: "11.03.2019",
+    plural: false
+  },
+  {
+    name: "John O'Brien",
+    date: "04.04.2019",
+    plural: false
+  }
 ];
 
-const listItems = data.map((row, index) => (
-  <li key={index}>
-    <Name name={row.name} date={row.date} plural={row.plural} />
-  </li>
+const listItems = data.map((row, index) => ( <
+  li key = {
+    index
+  } >
+  <
+  Name name = {
+    row.name
+  }
+  date = {
+    row.date
+  }
+  plural = {
+    row.plural
+  }
+  /> < /
+  li >
 ));
 
 class List extends React.Component {
   render() {
-    return (
-      <div className="ListHold">
-        <ul className="List" data-testid="List">
-          {listItems}
-        </ul>
-      </div>
+    return ( <
+      div className = "ListHold" >
+      <
+      ul className = "List"
+      data - testid = "List" > {
+        listItems
+      } <
+      /ul> < /
+      div >
     );
   }
 }
