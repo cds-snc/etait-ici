76599b0812558dfa417b6d41c36a23f4523590c7
Tim Arney <tim@line37.com>
2019-05-22 07:42:56 -0400

    push update

--- src/components/List.js
+++ src/components/List.js
@@ -23,7 +23,7 @@ class List extends React.Component {
     return (
       <div className="ListHold">
         <ul className="List" data-testid="List">
-          <div>K8 deploy v1</div>
+          <div>K8 deploy v2</div>
           {listItems}
         </ul>
       </div>
