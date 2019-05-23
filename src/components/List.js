d5c767de331ae03473a52a3830dcde005b78f561
Tim Arney <tim@line37.com>
2019-05-23 16:38:08 -0400

    update 3

--- src/components/List.js
+++ src/components/List.js
@@ -23,7 +23,7 @@ class List extends React.Component {
     return (
       <div className="ListHold">
         <ul className="List" data-testid="List">
-          <div>Concurrency 2</div>
+          <div>Concurrency 3</div>
           {listItems}
         </ul>
       </div>
