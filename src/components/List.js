f363ef4da58033beb215769d9d4b4468b20ac01d
Tim Arney <tim@line37.com>
2019-05-24 09:46:50 -0400

    test github status update

--- src/components/List.js
+++ src/components/List.js
@@ -23,7 +23,7 @@ class List extends React.Component {
     return (
       <div className="ListHold">
         <ul className="List" data-testid="List">
-          <div>List 7</div>
+          <div>List 8</div>
           {listItems}
         </ul>
       </div>
