33006f650b0aeaaa5461143ff838cae0ca0e4164
Tim Arney <tim@line37.com>
2019-05-29 13:51:50 -0400

    update

--- src/components/List.js
+++ src/components/List.js
@@ -23,7 +23,7 @@ class List extends React.Component {
     return (
       <div className="ListHold">
         <ul className="List" data-testid="List">
-          <div>Force Fail v2</div>
+          <div>Force Fail v2 - 1</div>
           {listItems}
         </ul>
       </div>
